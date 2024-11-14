c0511b7bf206b7ab6d1a3063634c8f3eb108eb7e
CarlosVF22 <cevasquezf22@gmail.com>
2024-11-14 17:31:47 -0500

    Actualizar configuración de Astro para usar Vercel Serverless

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import robotsTxt from "astro-robots-txt";
 
+import { defineConfig } from 'astro/config';
+// Import /serverless for a Serverless SSR site
+import vercelServerless from '@astrojs/vercel/serverless';
+
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
@@ -9,5 +13,5 @@ export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
   site: 'https://porfolio.dev/',
   output: "server",
-  adapter: vercel()
+  adapter: vercelServerless()
 });
