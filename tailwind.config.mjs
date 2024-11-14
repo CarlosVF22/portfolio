d9031bcb4c8e4b97a6978c87c893fe6c5f263347
CarlosVF22 <cevasquezf22@gmail.com>
2024-11-14 17:20:54 -0500

    firts commit for Carlos Vasquez's landing page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkMode: 'class',
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    darkMode: "class",
+    theme: {
+        extend: {
+            colors: {
+                red_personal_brand: "#A6033F",
+                pink_personal_brand: "#F20587",
+                pink_light_personal_brand: "#F205B3",
+                purple_personal_brand: "#8350F2",
+                purple_light_personal_brand: "#9E77F2",
+            },
+        },
+    },
+    plugins: [],
+};
