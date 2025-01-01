4f567c7e1c1a2e0b6411dcec44c49aa4badbab3d
CarlosVF22 <cevasquezf22@gmail.com>
2025-01-01 18:17:30 -0500

    agregando link de calendly

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import robotsTxt from "astro-robots-txt";
-// import vercel from "@astrojs/vercel";
-import sitemap from "@astrojs/sitemap";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind()],
+    integrations: [tailwind(), react()],
     site: "https://carlosvasquez.dev/",
     output: "static",
-    // adapter: vercel({}),
 });
